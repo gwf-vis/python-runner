f6bd7a48c74b44a434d7f5602418ea97e214864b
Sean Wong <seanwong24@live.com>
2021-12-16 14:31:21 +0000

    expand utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any, List, Union
 from flask import json
 import requests
 
@@ -6,7 +7,7 @@ SERVER_ENDPOINT = 'http://localhost:5000'
 
 
 def obtainFileContent(path: str):
-    fileUrl = f'{SERVER_ENDPOINT}/files/{path}'
+    fileUrl = os.path.join(SERVER_ENDPOINT, 'files', path)
     return requests.get(fileUrl).content.decode("utf-8")
 
 
@@ -23,3 +24,38 @@ def loadsJSON(data):
 
 def dumpsJSON(data):
     return json.dumps(data)
+
+
+def loadsDataIndex(path: str):
+    fileContent = obtainFileContent(path)
+    dataIndex = loadsJSON(fileContent)
+    dataIndex['directoryPath'] = os.path.dirname(path)
+    return dataIndex
+
+
+def loadsGeoData(dataIndex):
+    directoryPath = dataIndex['directoryPath']
+    geoJSONRelativePath = dataIndex['geoJSON']
+    fileContent = obtainFileContent(os.path.join(directoryPath, geoJSONRelativePath))
+    return loadsJSON(fileContent)
+
+def useMainPlugin(indexPath: str):
+    data = {}
+    data['pluginIndexUrl'] = indexPath
+    return data
+
+def setPluginProperty(toPlugin: dict, name: str, data):
+    if toPlugin is None:
+        return
+    toPlugin[name] = data
+
+def addPlugin(toPlugin: dict, name: str):
+    if toPlugin is None:
+        return
+    if('plugins' not in toPlugin):
+        toPlugin['plugins'] = []
+    plugin = {
+        'name': name
+    }
+    toPlugin['plugins'].append(plugin)
+    return plugin
