eb7d7637cfed4869d2002e0425165628d4595cb1
Sean Wong <seanwong24@live.com>
2022-08-21 04:24:58 +0000

    add some helpers for vis config

--- gwfvis.py
+++ gwfvis.py
@@ -6,6 +6,7 @@ from io import BytesIO
 
 SERVER_ENDPOINT = 'http://localhost:5000'
 
+
 def generate_vis(data):
     filePath = os.environ['RESULT_FILE_PATH']
     with open(filePath, 'w') as file:
@@ -20,6 +21,7 @@ def loads_json(data):
 def dumps_json(data):
     return json.dumps(data)
 
+
 def render_pyplot_figure():
     buffer = BytesIO()
     plt.savefig(buffer, format='png')
@@ -30,3 +32,53 @@ def render_pyplot_figure():
     b64String = b64.decode('utf-8')
     dataUrl = f'data:image/png;base64,{b64String}'
     print(dataUrl)
+
+# config helpers
+
+def load_default_vis_config():
+    return {
+        'imports': {
+            'data-fetcher': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-data-fetcher.js',
+            'tile-layer': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-tile-layer.js',
+            'geojson-layer': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-geojson-layer.js',
+            'user-selection': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-selection.js',
+            'metadata': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-metadata.js',
+            'line-chart': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-line-chart.js',
+            'radar-chart': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-radar-chart.js',
+            'dimension-control': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-dimension-control.js',
+            'variable-control': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-variable-control.js',
+            'legend': 'http://gwfvis.usask.ca/RiverFlow/api/file/fetch/public/plugins/default/gwf-vis-plugin-legend.js'
+        },
+        'plugins': {
+            'data': {
+                'name': 'data-fetcher',
+                'props': {
+                    'sqliteWorkerUrl': 'http://gwfvis.usask.ca/RiverFlow/assets/sqljs/worker.sql-wasm.js'
+                }
+            },
+        }
+    }
+
+def add_plugin(vis_config, container, plugin_config):
+    plugins = vis_config['plugins']
+    if plugins is None:
+        vis_config['plugins'] = {}
+    if container == 'data':
+        plugins['data'] = plugin_config
+    else:
+        plugin_container = plugins[container]
+        if plugin_container is None:
+            plugin_container = plugins[container] = []
+        plugin_container.append(plugin_config)
+
+DEFAULT_SATELITTE_LAYER = {
+    'name': 'tile-layer',
+    'props': {
+        'name': 'Satelitte',
+        'type': 'base-layer',
+        'urlTemplate': 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'options': {
+            'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        }
+    }
+}
